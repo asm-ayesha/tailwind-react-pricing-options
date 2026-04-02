48ce72f57336cd859a4ed6e676fd87960f7d5aab
asm-ayesha <ayesha219333@gmail.com>
2026-04-02 11:30:21 +0600

    dynamic menu button toggle and menu toggle

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,39 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
+
+
 
 
 const navigationData = [
-  { id: 1, name: "Home",        path: "/home",             method: "GET"  },
-  { id: 2, name: "Users",       path: "/api/users",        method: "GET"  },
-  { id: 3, name: "Create User", path: "/api/users/create", method: "POST" },
-  { id: 4, name: "Products",    path: "/api/products",     method: "GET"  },
-  { id: 5, name: "Settings",    path: "/settings",         method: "PUT"  },
+    { id: 1, name: "Home", path: "/home", method: "GET" },
+    { id: 2, name: "Users", path: "/api/users", method: "GET" },
+    { id: 3, name: "Create User", path: "/api/users/create", method: "POST" },
+    { id: 4, name: "Products", path: "/api/products", method: "GET" },
+    { id: 5, name: "Settings", path: "/settings", method: "PUT" },
 ];
 
 const NavBar = () => {
+    const [open, setOpen] =useState(false)
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
     return (
-        <nav>
-
-           <ul className='flex justify-center gap-5'>
-             {
-                navigationData.map(route => <Link key={route.id} route={route}></Link>)
-            }
-            
-           </ul>
-            {/* <ul className='flex justify-center gap-5'>
+        <nav className='flex justify-between mx-10'>
+            <div className='flex' onClick={() => setOpen(!open)}>
                 {
-                    navigationData.map(route => <li><a href={route.path}>{route.name}</a></li>)
+                    open? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu> 
                 }
-            </ul> */}
 
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+               
+            <h3 className='ml-4'>My Navbar</h3>
+            </div>
 
+            <ul className='md:flex justify-center gap-5 hidden'>
+                {
+                    links
+                }
 
-
-            {/* <ul className='flex justify-center gap-5'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Blog</li>
-            </ul> */}
+            </ul>
+          
+            <button>Sign in</button>
         </nav>
     );
 };
